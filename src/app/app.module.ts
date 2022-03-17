a461952d3d58ac04b44ffe6bc9a32a738ddf3c11
Marquinhos Oliveira <marquinhosadventista@gmail.com>
2022-03-17 03:22:55 -0300

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { ProdutoComponent } from './views/produto/produto.component';
 import { ProdutoFormComponent } from './components/produto/produto-form/produto-form.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDividerModule} from '@angular/material/divider';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatTableModule} from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { StorageService } from './services/storage.service';
+import { TodoListService } from './services/todo-list.service';
 
 @NgModule({
   declarations: [
@@ -53,7 +55,10 @@ import {MatTableModule} from '@angular/material/table';
 
 
   ],
-  providers: [],
+  providers: [
+    TodoListService,
+    StorageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
